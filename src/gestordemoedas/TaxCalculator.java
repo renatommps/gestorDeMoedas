a1044ced0c4954963a593ccfdb1bedd36f55b8e5
18100871 <18100871@ine>
2018-04-02 19:49:52 -0300

    bla blav

--- src/gestordemoedas/TaxCalculator.java
+++ src/gestordemoedas/TaxCalculator.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestordemoedas;
 
-/**
- *
- * @author r.barbaresco
- */
+
 public class TaxCalculator {
-    
-    
-    
-    
+     
     public double tax(Coin c) {
         double corretagem = 0.0025;
         double iof = 0;
